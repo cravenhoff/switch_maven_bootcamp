b80d49c0fded0d8b20cac014cd6f0812836e4583
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-07 13:52:06 +1000

    Create string_quotes.js

---
+++ classes/6. Day 06 - 7.09.2020/JavaScript.Info/JavaScript Fundamentals/Lesson 2.5/string_quotes.js
@@ -0,0 +1,20 @@
+/*
+
+  String Quotes
+  
+  Instructions:
+  
+  What is the output of the script?
+
+  let name = "Ilya";
+  alert( `hello ${1}` ); // ?
+  alert( `hello ${"name"}` ); // ?
+  alert( `hello ${name}` ); // ?
+  
+  Responses:
+  
+  => Alert function outputs the string: hello 1.
+  => Alert function outputs the string: hello "Ilya". Got this one wrong. Ran the function in the console and got this instead: hello name.
+  => Alert function outputs the string: hello Ilya.
+
+*/
