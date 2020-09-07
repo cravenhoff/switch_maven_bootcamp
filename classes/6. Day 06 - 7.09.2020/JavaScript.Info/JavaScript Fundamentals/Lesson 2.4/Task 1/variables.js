8bafb8c594e2901ec87126bd4dbabd57372f4c95
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-07 11:45:59 +1000

    Create variables.js

---
+++ classes/6. Day 06 - 7.09.2020/JavaScript.Info/JavaScript Fundamentals/Lesson 2.4/Task 1/variables.js
@@ -0,0 +1,17 @@
+/*
+  Working with variables
+  
+  Instructions:
+  1. Declare two variables: admin and name.
+  2. Assign the value "John" to name.
+  3. Copy the value from name to admin.
+  4. Show the value of admin using alert (must output “John”).
+*/
+
+// Declare variables: admin and name
+let name = "John";
+
+let admin = name; // Copy the value of name to admin
+
+// Output the value of admin using alert()
+alert(admin);
