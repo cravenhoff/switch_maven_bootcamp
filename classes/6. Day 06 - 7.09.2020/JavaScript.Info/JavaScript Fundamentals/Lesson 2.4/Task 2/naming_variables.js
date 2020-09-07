3f7f1e4be29f143371c450407d73f8989cdfbadc
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-07 11:50:19 +1000

    Create naming_variables.js

---
+++ classes/6. Day 06 - 7.09.2020/JavaScript.Info/JavaScript Fundamentals/Lesson 2.4/Task 2/naming_variables.js
@@ -0,0 +1,15 @@
+/*
+
+  Giving the Right Name
+  
+  Instructions:
+  1. Create a variable with the name of our planet. How would you name such a variable?
+  2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+
+*/
+
+// Variable for planet earth
+let ourPlanet = "Earth";
+
+// Variable for current website visitor
+let currentVistor = "Sam"; 
