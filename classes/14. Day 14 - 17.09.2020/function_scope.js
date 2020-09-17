ab91d6344cb417c958baa7f2eeb3aaebeb787e1c
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-17 10:51:01 +1000

    Comments
    Added some comments on variable, scope and function declaration and calling.

--- classes/14. Day 14 - 17.09.2020/function_scope.js
+++ classes/14. Day 14 - 17.09.2020/function_scope.js
@@ -2,17 +2,21 @@
 
 // Exercise #01:
 
-let hero = "mike";
+let hero = "mike"; // Definition of global variable "hero".
 
+// Definition of outer() function.
 let outer = function() {
-  let verb = "eats";
+  let verb = "eats"; // Definition of local variable "verb".
   
+  // Definition of inner() function
   let inner = function(food) {
      console.log(`${hero} ${verb} ${food}`);
   }
   
+  // inner() function is invoked
   inner("pizza");
   inner("a lot");
 }
 
+// outer() function is invoked.
 outer();
