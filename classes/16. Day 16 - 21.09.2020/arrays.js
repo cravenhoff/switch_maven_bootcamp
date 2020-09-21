ce7c2d495ed2c87297e9b34a714346e0f00fd136
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-21 14:37:06 +1000

    Create arrays.js
    Creating arrays in JavaScript.

---
+++ classes/16. Day 16 - 21.09.2020/arrays.js
@@ -0,0 +1,11 @@
+/* ARRAYS */
+
+/*
+
+  => Creating Arrays
+  Arrays are created like any other ordinary variable. The only difference is that arrays are distinguished using the square bracket symbol: []. Within the square bracket, array elements are added, each of which is separated by a comma.
+
+*/
+
+// Create a fruits array
+let fruits = ["apple", "orange", "pair", "banana"];
