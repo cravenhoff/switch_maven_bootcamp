63f3398992ea9ebd08f1e95a188ab53929be7189
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-21 14:42:53 +1000

    Accessing Arrays Elements
    How to access the contents (elements) of arrays using bracket notation and indexes.

--- classes/16. Day 16 - 21.09.2020/arrays.js
+++ classes/16. Day 16 - 21.09.2020/arrays.js
@@ -4,8 +4,19 @@
 
   => Creating Arrays
   Arrays are created like any other ordinary variable. The only difference is that arrays are distinguished using the square bracket symbol: []. Within the square bracket, array elements are added, each of which is separated by a comma.
-
+  
+  => Accessing Arrays
+  Array elements are accessed using index values. Arrays are structured using a key-value pair or relationship, where each element (or value) is assigned to a specific index (key).
+  Array elements start with an index of 0, and not 1. Hence, the first element of an array will have an index of 0, the second will have an index of 1, and so on and so forth.
+  Elements are accessed using bracket notation, where the index position of the element to retrieve, is enclosed within the bracket.
+  
 */
 
 // Create a fruits array
 let fruits = ["apple", "orange", "pair", "banana"];
+
+// Access the first element of the fruits array
+fruits[0]; // Retrieves the first element and returns its value => apple.
+
+// Saving an element to a variable
+let orange = fruits[1];
