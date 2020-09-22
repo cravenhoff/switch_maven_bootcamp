5bf2913ea029063dbc26c76987c00e2a1a704007
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-23 07:37:43 +1000

    Create patterns.js

---
+++ classes/17. Day 17 - 22.09.2020/patterns.js
@@ -0,0 +1,8 @@
+/* CREATING PATTERNS (STRING CHARACTERS) USING LOOPS */
+
+// First Pattern: Create a pattern using hashes or aesterisks that stars at one and increases by a value of one.
+// Second Pattern: Same as above, but the pattern this time should be inverted.
+// Third Pattern: Create a similar pattern as the "first pattern", but have it right-aligned.
+// Fourth Pattern: Same as pattern three, but this time the pattern should be inverted.
+// Fifth Pattern: Create a pattern that prints out a symmetrical pyramid.
+// Sixth Pattern: Create a pattern that prints out a christmas tree. The tree should have a trunk as well.
