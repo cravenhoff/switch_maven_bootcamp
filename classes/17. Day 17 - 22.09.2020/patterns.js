7e75ac51d938b28f61eb88b7ebb6eaf7fb95bae1
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-23 08:24:41 +1000

    Second Pattern
    First implementation and solution of the second pattern.

--- classes/17. Day 17 - 22.09.2020/patterns.js
+++ classes/17. Day 17 - 22.09.2020/patterns.js
@@ -2,17 +2,29 @@
 
 // First Pattern: Create a pattern using hashes or aesterisks that stars at one and increases by a value of one.
 let hash = "";
-let innerIdx = 5;
-let outerIdx = 5;
-let counter = 0;
 
-for (let outerCounter = 1; outerCounter <= 5; outerCounter++) {
+for (let counter = 1; counter <= 5; counter++) {
   console.log(hash + "#");
   hash += "#";
 }
 
 
 // Second Pattern: Same as above, but the pattern this time should be inverted.
+// Solution / Implement #1:
+let char = "#";
+let charNum = 5;
+
+for (let outerCounter = 1; outerCounter <= 1; outerCounter++) {
+  char = char.repeat(charNum);
+  
+  for (let innerCounter = 1; innerCounter <=5; innerCounter++) {
+    console.log(char);
+    char = char.slice(0, char.length-1);
+    charNum--;
+  }
+}
+
+
 // Third Pattern: Create a similar pattern as the "first pattern", but have it right-aligned.
 // Fourth Pattern: Same as pattern three, but this time the pattern should be inverted.
 // Fifth Pattern: Create a pattern that prints out a symmetrical pyramid.
