c3e67bfd308176b5e062773face940c2da9b7bef
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-23 08:31:23 +1000

    Alternative Solutions: First Pattern
    Added alternative solutions and implementations of the first pattern using while and for loops.

--- classes/17. Day 17 - 22.09.2020/patterns.js
+++ classes/17. Day 17 - 22.09.2020/patterns.js
@@ -1,16 +1,35 @@
 /* CREATING PATTERNS (STRING CHARACTERS) USING LOOPS */
 
-// First Pattern: Create a pattern using hashes or aesterisks that stars at one and increases by a value of one.
+/* First Pattern: Create a pattern using hashes or aesterisks that stars at one and increases by a value of one. */
+// Solution / Implementation #1:
 let hash = "";
+let counter = 0;
 
 for (let counter = 1; counter <= 5; counter++) {
   console.log(hash + "#");
   hash += "#";
 }
 
+// Solution / Implementation #2: Using a While Loop
+while (counter < 5) {
+	console.log(hash + "#");
+  hash += "#";
+	counter++;
+}
 
-// Second Pattern: Same as above, but the pattern this time should be inverted.
-// Solution / Implement #1:
+// Solution / Implementation #3: Using a While and For Loop
+while (counter < 2) {
+	for (i = 0; i < 3; i++) {
+		console.log(hash + "#");
+        hash += "#";
+	}
+  
+	counter++;
+}
+
+
+/* Second Pattern: Same as above, but the pattern this time should be inverted. */
+// Solution / Implementation #1:
 let char = "#";
 let charNum = 5;
 
