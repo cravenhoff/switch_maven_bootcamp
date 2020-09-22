4f2397c57a1017c0ec9f4f92d702ec034aa3a953
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-23 07:44:50 +1000

    First Pattern
    How to create / generate the first pattern using a single for loop.

--- classes/17. Day 17 - 22.09.2020/patterns.js
+++ classes/17. Day 17 - 22.09.2020/patterns.js
@@ -1,6 +1,17 @@
 /* CREATING PATTERNS (STRING CHARACTERS) USING LOOPS */
 
 // First Pattern: Create a pattern using hashes or aesterisks that stars at one and increases by a value of one.
+let hash = "";
+let innerIdx = 5;
+let outerIdx = 5;
+let counter = 0;
+
+for (let outerCounter = 1; outerCounter <= 5; outerCounter++) {
+  console.log(hash + "#");
+  hash += "#";
+}
+
+
 // Second Pattern: Same as above, but the pattern this time should be inverted.
 // Third Pattern: Create a similar pattern as the "first pattern", but have it right-aligned.
 // Fourth Pattern: Same as pattern three, but this time the pattern should be inverted.
