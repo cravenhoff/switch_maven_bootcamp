a7a20788142dffcb2f819cf15b9b22e9bde51557
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-23 21:10:31 +1000

    Passing Function Arguments
    Created a wrapper function and passed a function as its arguments.

--- classes/18. Day 18 - 23.09.2020/function_arguments.js
+++ classes/18. Day 18 - 23.09.2020/function_arguments.js
@@ -9,3 +9,11 @@ function hello() {
 // Save the hello() function in a new variable.
 let x = hello();
 x; // Calls the x variable, which executes or "invokes" the hello() function.
+
+// Create a new function that executes the hello() function by passing it as an argument.
+function wrapper(funcHello) {
+  funcHello(); // Invokes the function passed as an argument.
+}
+
+// Invoke the wrapper() and hello() functions.
+wrapper(hello);
