5b45a9770c6c77161a29a7f77e0da248ff7d104d
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-27 21:17:05 +1000

    JS Functions
    Introduction to JavaScript functions and some key concepts, crucial to understanding functions.

--- classes/20. Day 20 - 25.09.2020/recursion.js
+++ classes/20. Day 20 - 25.09.2020/recursion.js
@@ -1,1 +1,28 @@
 /* WORKING WITH RECURSIONS IN JAVASCRIPT */
+
+/*
+
+  => JavaScript Functions
+  JavaScript deals with different types of functions, but more often in your programming career, you are likely to come across the two common ones listed below:
+  1. Functions
+  2. Recursions
+  
+  => Functions
+  Functions in JavaScript are essentially "sub-programs" that group relatable code together and provide a clean, repeatable method for executing a single feature or operation in an application. In short, a JavaScript function is a block of code designed to perform a particular task.
+  Functions are created using the "function" keyword, accept inputs through the use of parameters, and have a return value, either explicity stated or not (in this case, the function returns a value of "undefined" by default.
+  Functions are called by using their name, followed by parenthesis. This is called "invoking" a function, which instructs the JavaScript engine to "process" or "execute" the code within the functions curly braces.
+  
+  => Function Expression
+  A function expression defines a named or anomymous function.
+  Eg: let greet = function(name) {
+        console.log("Hello, " + name);
+      }
+  
+  => Types of Functions in JavaScript:
+ JavaScript provides the following types of functions:
+ 1. Named Functions
+ 2. Anonymous Functions
+  
+  => Recursions
+
+*/
