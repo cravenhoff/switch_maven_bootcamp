679cbe8ba20cafda0819b84ae6a59ad2fe39330b
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-28 11:49:10 +1000

    Passing in Objects to Functions
    Exploring the passing in of objects as arguments to parameters, without the "this" variable.

--- classes/21. Day 21 - 28.09.2020/this_variable.js
+++ classes/21. Day 21 - 28.09.2020/this_variable.js
@@ -13,3 +13,17 @@ let obj = {
 
 // Call the property function inc()
 obj.inc(); // Returns the increment of 1.
+
+// Without the "this" keyword, the above function will be as follows
+let newObj = {
+  count: 0,
+  increase: inc
+}
+
+function inc(objData) {
+  return objData.count += 1;
+}
+
+inc(newObj); // Passes in the newObj{} object
+
+
