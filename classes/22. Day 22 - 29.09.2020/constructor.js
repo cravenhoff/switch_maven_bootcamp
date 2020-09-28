631b26fd461c0335e917d419d55a1d99daacb8ec
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-29 09:54:48 +1000

    Create constructor.js

---
+++ classes/22. Day 22 - 29.09.2020/constructor.js
@@ -0,0 +1,10 @@
+/* OBJECT INSTANTIATION AND JAVASCRIPT FUNCTION CONSTRUCTORS */
+
+/*
+
+  OUTLINE:
+  1. Constructor Function
+  2. Using the Call() Object Method
+  3. Referencing the Global Window Object
+
+*/
