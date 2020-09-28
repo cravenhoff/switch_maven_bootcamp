d517024264e130ef42668a69b17b65957ca9718f
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-28 14:40:28 +1000

    this in Objects Functions
    How to work with "this" in object functions.

--- classes/21. Day 21 - 28.09.2020/this_variable.js
+++ classes/21. Day 21 - 28.09.2020/this_variable.js
@@ -6,6 +6,11 @@
   Used to reference objects in a way that does not hard-code the objects the "this" keyword should reference and look up. Furthermore, it allows for one function to be created and used as many times as required by different objects throughout the life of the program.
   
   When the "this" keyword is used within a function, the object does not need to be passed into the function for it to be accessible to the "this" variable. JavaScript automatically takes care of that. However, this can only be done when the function referencing the object using "this" is linked as a property of the (soon to be referenced) object.
+  
+  => "this" in Object Functions
+  Functions declared as values of properties can reference their parent object using the "this" keyword. In such instances, the parent object is the "owner" of the function property, and therefore whenever the function uses the "this" keyword, it is referencing its parent object.
+  
+  Example: Refer to the person{} object below.
 
 */
 
@@ -53,3 +58,14 @@ function countObjs(that) {
 countObjs(objOne); // ObjOne.count === 1
 objOne.count; // Returns 1
 objTwo.count; // Returns 0.
+
+// person object
+var person = {
+  firstName:"John",
+  lastName: "Doe",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+person.fullName(); // Returns "John Doe"
