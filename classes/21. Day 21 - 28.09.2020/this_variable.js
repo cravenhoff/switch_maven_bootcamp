950353cd4002b1667b2c2b636661e4af669cbe5c
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-28 11:02:19 +1000

    this Example Code
    Putting the "this" concept into practice.

--- classes/21. Day 21 - 28.09.2020/this_variable.js
+++ classes/21. Day 21 - 28.09.2020/this_variable.js
@@ -1,1 +1,15 @@
 /* WORKING WITH THE "THIS" VARIABLE KEYWORD IN JAVASCRIPT */
+
+// Example code of the "this" keyword
+function increase() {
+  return this.count += 1;
+}
+
+// Create the object the "this" keyword will reference
+let obj = {
+  count: 0,
+  inc: increase
+}
+
+// Call the property function inc()
+obj.inc(); // Returns the increment of 1.
