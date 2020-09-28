6725fbb99e250f8f356210a5a4f037249251ae40
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-29 08:07:37 +1000

    Create recursion_exercises.js

---
+++ classes/21. Day 21 - 28.09.2020/recursion_exercises.js
@@ -0,0 +1,40 @@
+/* LIST OF RECURSIONS ALGORITHMS AND EXERCISES */
+// Following are a list of sample recursion algorithms and exercises I worked on during the course of the Switch Maven Software Development Bootcamp.
+
+/*
+  FREECODECAMP EXERCISE:
+  1. Write a program that reverses a string using recursion. Given the string "freeCodeCamp" your program should return "pmaCedoCeerf".
+*/
+
+let counter = 0;
+let reversedChar = [];
+let reversedStr = "";
+let finalStr = "";
+
+let reverseString = str => {
+	let stringLength = str.length;
+
+	// Define base camp
+	if (stringLength === 0) {
+		return "You've run out of characters!";
+	} else {
+		let strChar = str[stringLength - 1]
+		console.log(strChar);
+
+		let newStr = "";
+		newStr += str.slice(counter, stringLength - 1);
+
+		reversedChar.push(strChar);
+
+		reversedStr = reversedChar.toString();
+
+		finalStr = reversedStr.replace(",", "");
+
+		return reverseString(newStr);
+	}
+
+	counter += 1;
+	console.log(finalStr);
+}
+
+console.log(reverseString("freeCodeCamp"));
