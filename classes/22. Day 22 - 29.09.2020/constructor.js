bb7e46d1578f9aa630ba6b8167f2557a6e027411
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-29 10:10:55 +1000

    Clone Constructor Function: Crystal
    Created a clone constructor function that can instantiate new object clones of myself.

--- classes/22. Day 22 - 29.09.2020/constructor.js
+++ classes/22. Day 22 - 29.09.2020/constructor.js
@@ -3,8 +3,24 @@
 /*
 
   OUTLINE:
-  1. Constructor Function
+  => Constructor Function
+  A function that acts as a constructor in JavaScript for creating or "instantiating" new objects dynamically, without the need to independently create objects or using a loop to assign the same properties to different objects.
+  
+  A constructor function utilizes the "this" keyword to reference new instantiated objects, and assigns properties to it using dot notation. Generally, the constructor will accept the property values as arguments.
+ 
   2. Using the Call() Object Method
   3. Referencing the Global Window Object
 
 */
+
+// Create a "clone" constructor function that instantiates new object clones of yourself
+let crystalClone = function (name, age, superPower) {
+  this.name = name;
+  this.age = age;
+  this.superPower = superPower;
+}
+
+// Instantiate a clone of Crystal
+let crystalClone1 = new crystalClone("Crystal", 22, "Time Travel");
+let crystalClone2 = new crystalClone("Craven", 12, "Teleportation");
+let crystalClone3 = new crystalClone("Lenora", 33, "Photographic Memory");
