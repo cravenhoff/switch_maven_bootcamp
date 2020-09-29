3f2214d26eff54d39984b9a4dc4bce8caf9c01f4
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-09-30 09:53:23 +1000

    Create event_listeners.js

---
+++ classes/23. Day 23 - 30.09.2020/event_listeners.js
@@ -0,0 +1,10 @@
+/* EVENTS LISTENERS IN JAVASCRIPT */
+
+/*
+
+  => Event Listeners
+  Event listeners run in the browser and "listen out" for specific, predefined events of the mouse. The event listener will trigger a specific function (or program) to run "when" an event is triggered.
+  Syntax: webpageComponent.addEventListener(type, func)
+  Syntax: webpageComponent.addEventListener(eventTypeToListenOutFor, functionToRunWhenEventIsTrigger)
+
+*/
