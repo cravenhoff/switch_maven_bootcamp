1270db06d8c15c46169e276a9fc6ec1d1c08a4de
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-10-02 10:00:38 +1000

    Event Loop Structure
    Basic introduction to event loops and their architecture.

--- classes/24. Day 24 - 1.10.2020/event_loop.js
+++ classes/24. Day 24 - 1.10.2020/event_loop.js
@@ -3,5 +3,11 @@
 /*
 
    => The Event Loop
+   The event loop is a JavaScript mechanism that helps to keep track of events in the browser window and user interactions.
+   
+   The event loop is managed by the browser and consists of the following three core components:
+   1. Event Listeners Container
+   2. The Event Queue
+   3. Stack
 
 */
