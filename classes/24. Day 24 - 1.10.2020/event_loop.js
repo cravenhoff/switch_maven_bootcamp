dbcc75ccb962d43f42840607866ec48ad43c7e75
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-10-02 10:07:10 +1000

    Event Listeners Container
    A summary or introduction to the Event Listeners Storage / Container - the first key component of the Event Loop Mechanism.

--- classes/24. Day 24 - 1.10.2020/event_loop.js
+++ classes/24. Day 24 - 1.10.2020/event_loop.js
@@ -9,5 +9,13 @@
    1. Event Listeners Container
    2. The Event Queue
    3. Stack
+   
+   => Event Listeners Storage / Container
+   The event listeners container keeps track of and "stores" every registered event listener created in the program.
+   These event listeners are tracked by the JavaScript Browser and continuously fed to it or "broadcasted".
+   Basically, anytime the browser comes across an event listener in the code, it throws it (or stores it) into the Event Listeners Container.
+   
+   Eg: htmlElement.addEventListener("event_type", "function_name");
+   In the above example, the event_type specified in the event listener, is tied to a function that's invoked when that particular event is triggered on the htmlElement specified.
 
 */
