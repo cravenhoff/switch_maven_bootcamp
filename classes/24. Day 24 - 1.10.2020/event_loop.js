0f7b3192118c2c347cc7df2f8435a66b15837882
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-10-02 10:15:15 +1000

    The Event Queue
    Brief introduction to the Event Queue - what it is and what it does.

--- classes/24. Day 24 - 1.10.2020/event_loop.js
+++ classes/24. Day 24 - 1.10.2020/event_loop.js
@@ -17,5 +17,10 @@
    
    Eg: htmlElement.addEventListener("event_type", "function_name");
    In the above example, the event_type specified in the event listener, is tied to a function that's invoked when that particular event is triggered on the htmlElement specified.
+   
+   => The Event Queue
+   The event queue is the second key component of the Event Loop Mechanism, and is responsible for, as the name suggests, "queueing" event listeners (stored in the event listeners container) as and when they are triggered. In precise terms, the event queue holds the "function" that is to be executed when a specified event goes off. In the event listener example above, this function is the "function_name" provided in the event listener code/syntax.
+   
+   All the triggered events are "scheduled" in the event queue, waiting to be run when the "stack" (call stack) is empty and cleared out.
 
 */
