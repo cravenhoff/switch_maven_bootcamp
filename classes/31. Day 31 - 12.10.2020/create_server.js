9b943f3547dfd69b6fc59531d844aa5a6ba7b973
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-10-12 23:52:33 +1000

    Status Code and Header Responses
    Set the status code and header responses.

--- classes/31. Day 31 - 12.10.2020/create_server.js
+++ classes/31. Day 31 - 12.10.2020/create_server.js
@@ -9,5 +9,10 @@ const port = 3120;
 // Create server
 const server = http.createServer(function(res, req) {
   console.log("Our server was a hit!");
-  console.log(`Here's our URL: ${res.url}`);
+  console.log(res.url);  // console.log(`Here's our URL: ${res.url}`);
+  
+  // Set status code and header
+  res.statusCode = 200;
+  res.setHeader = ("Content-Type", "text/html");
+  
 })
