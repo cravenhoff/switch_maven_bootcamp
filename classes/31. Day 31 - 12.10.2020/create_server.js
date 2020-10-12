ff3ffd42f6a541e28edb00f81df21156ef09f306
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-10-12 23:47:46 +1000

    Initial Variables - HTTP, Host Name and Port
    Imported the HTTP Object and created constants (variables) for the host name and the port.

--- classes/31. Day 31 - 12.10.2020/create_server.js
+++ classes/31. Day 31 - 12.10.2020/create_server.js
@@ -1,1 +1,7 @@
 /* Creating a Local Server With Node.js */
+// Import the "http" object
+const http = require("http");
+
+// Create constants for hostname and port
+const hostName = "127.0.01";
+const port = 3120;
